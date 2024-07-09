ccaa43602d90264ecdf3e35bfb8941064e72b7ec
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-07-09 17:46:53 +0530

    img table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # models.py
-from sqlalchemy import Column, Integer, TIMESTAMP, Boolean, String, LargeBinary
+from sqlalchemy import Column, Integer, TIMESTAMP, Boolean, String, LargeBinary, Text
 from database import Base
 
 class Fabric(Base):
@@ -9,4 +9,12 @@ class Fabric(Base):
     date = Column(TIMESTAMP(), index=True)
     status = Column(Boolean(),  index=True)
     file = Column(String(100), index=True)
+
+class FabricImage(Base):
+    __tablename__ = "production_images"
+
+    id = Column(Integer, primary_key=True, index=True)
+    input_image = Column(Text(length=16777215))  # MEDIUMTEXT
+    output_image = Column(Text(length=16777215))  # MEDIUMTEXT
+    history_id = Column(Integer, index=True, unique=True)
 
